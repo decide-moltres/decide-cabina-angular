3a8792e49fa865286deed2016587d05d22cba918
José Manuel de los Reyes <josedelosreyesgarcia@gmail.com>
2019-12-11 14:01:26 +0100

    Added routing (feat) The app now supports routing
    Close #11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { VotingComponent } from './voting/voting.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'votings', component: VotingComponent },
+  { path: 'votings/:id', component: VotingComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
