4e6336f1ad6beb9826fac742bd656918ad98b275
José Manuel de los Reyes <josedelosreyesgarcia@gmail.com>
2019-12-11 16:06:40 +0100

    Updated app and header components (feat) Added some authentication logic to these components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from './services/authentication.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'decide-cabina-angular';
+export class AppComponent implements OnInit {
+  token: string;
+
+  constructor(private authService: AuthenticationService) { }
+
+  ngOnInit() {
+    this.token = this.authService.getToken();
+    console.log(this.token);
+
+    if (this.token || this.token !== '') {
+      this.authService.getUser(this.token).subscribe((res) => {
+        console.log(res);
+        this.authService.changeLoggedStatus(true);
+      }, (error) => {
+        console.log(error);
+        this.authService.changeLoggedStatus(false);
+        this.authService.removeToken();
+      });
+    } else {
+      this.authService.changeLoggedStatus(false);
+    }
+  }
 }
