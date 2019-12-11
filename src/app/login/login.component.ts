36a6b2c6245894052722a35ad04480ceb224d7c1
Christian.martinez <chrmarand@alum.us.es>
2019-12-11 17:15:58 +0100

    Modified login form (feat) Added redirection to home page when user logs in properly
    References #16

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,7 @@ export class LoginComponent implements OnInit {
   submitted: boolean;
   loading: boolean;
 
-  constructor(private authService: AuthenticationService) { }
+  constructor(private authService: AuthenticationService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -32,12 +33,13 @@ export class LoginComponent implements OnInit {
         if (res.hasOwnProperty('token')) {
           this.authService.setToken((res as any).token);
           this.authService.changeLoggedStatus(true);
+          this.router.navigate(['']);
         } else {
           this.authService.changeLoggedStatus(false);
         }
       },
       error => {
-        console.error(error);
+        // console.error(error);
         this.authService.changeLoggedStatus(false);
         this.loading = false;
       });
