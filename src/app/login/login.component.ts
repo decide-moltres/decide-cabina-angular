7e564b35620751131b3f32dce5405b67b138357e
rubbuemen <ruben_077@msn.com>
2019-12-11 16:49:02 +0100

    Added login form (feat) Added login form and changed some styles from app component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  submitted: boolean;
+  loading: boolean;
 
-  constructor() { }
+  constructor(private authService: AuthenticationService) { }
 
   ngOnInit() {
+  }
+
+  onSubmit(username: string, password: string, event: Event) {
+    event.preventDefault();
+
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (username === '' || password === '') {
+      return;
+    }
+
+    this.loading = true;
+    this.authService.login(username, password).subscribe(
+      res => {
+        console.log(res);
+        if (res.hasOwnProperty('token')) {
+          this.authService.setToken((res as any).token);
+          this.authService.changeLoggedStatus(true);
+        } else {
+          this.authService.changeLoggedStatus(false);
+        }
+      },
+      error => {
+        console.error(error);
+        this.authService.changeLoggedStatus(false);
+        this.loading = false;
+      });
   }
 
 }
