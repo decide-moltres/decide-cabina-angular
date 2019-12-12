81958ade38d58fcfa6549005835d22d047df0d81
rubbuemen <ruben_077@msn.com>
2019-12-12 13:41:33 +0100

    Fixed votings route (fix) Fixed the component to be loaded when accessed to votings URL. Before, it was loading VotingComponent instead of VotingsComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { VotingComponent } from './voting/voting.component';
+import { VotingsComponent } from './votings/votings.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'votings', component: VotingComponent },
+  { path: 'votings', component: VotingsComponent },
   { path: 'votings/:id', component: VotingComponent }
 ];
 
