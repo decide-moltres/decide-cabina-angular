86326c411c13693b1b22c8ef2b6e53eed1f303a0
Christian.martinez <chrmarand@alum.us.es>
2020-01-04 00:36:39 +0100

    Increase voting functionality (feat) Added services and changes on models to do the post method to Decide
    Reference #25

--- src/app/models/voting.model.ts
+++ src/app/models/voting.model.ts
@@ -1,11 +1,12 @@
 import { Question } from './question.model';
 
 export class Voting {
-    constructor(
-        public id: number,
-        public name: string,
-        public desc: string,
-        public question: Question,
-        public startDate: Date,
-        public endDate: Date) { }
+  constructor(
+    public id: number,
+    public name: string,
+    public desc: string,
+    public question: Question,
+    public startDate: Date,
+    public endDate: Date,
+    public pubKey: []) { }
 }
