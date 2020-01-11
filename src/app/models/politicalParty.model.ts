8dbc491b7a181426ac038ffea3e2bb950f7c7717
rubbuemen <ruben_077@msn.com>
2020-01-11 19:37:50 +0100

    Added PoliticalParty model (feat) Added PoliticalParty model since it is needed by integration
    Rerefences #44

---
+++ src/app/models/politicalParty.model.ts
@@ -0,0 +1,9 @@
+export class PoliticalParty {
+  constructor(
+    public name: string = '',
+    public acronym: string = '',
+    public description: string = '',
+    public headquarters: string = '',
+    public image: string = '',
+    public president: string = '') { }
+}
