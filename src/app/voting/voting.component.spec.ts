b6afe5573d2dd6ba7ddd4cdfb1014cecc8e50e5e
christian-martinez-andrades <chrmarand@alum.us.es>
2020-01-12 15:19:56 +0100

    Created voting test (test)
    New test added. This test checks if the component was created correctly
    
    References #34

--- src/app/voting/voting.component.spec.ts
+++ src/app/voting/voting.component.spec.ts
@@ -1,7 +1,29 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { VotingComponent } from './voting.component';
-import { RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
 
+describe('VotingComponent', () => {
+    let component: VotingComponent;
+    let fixture: ComponentFixture<VotingComponent>;
+    let element: HTMLElement;
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [VotingComponent],
+            imports: [FormsModule, RouterTestingModule, HttpClientTestingModule],
+        })
+            .compileComponents();
+    }));
 
+    beforeEach(() => {
+        fixture = TestBed.createComponent(VotingComponent);
+        component = fixture.componentInstance;
+        element = fixture.nativeElement;
+        fixture.detectChanges();
+    });
 
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
