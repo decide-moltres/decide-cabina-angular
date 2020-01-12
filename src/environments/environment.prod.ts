4de7804fc49cb7a76826a0a0d3e4e1164123555d
rubbuemen <ruben_077@msn.com>
2020-01-12 18:16:16 +0100

    Changed api url (config) Now the api url is the common one of the organization

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://decide-heroku-123.herokuapp.com/',
+  apiUrl: 'https://egc-decide-moltres.herokuapp.com/',
   appUrl: 'https://decide-angular.herokuapp.com/'
 };
