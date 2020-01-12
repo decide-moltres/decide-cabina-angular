0ff6036b165b15f689d75480358ff4c2c91dcf5e
Abdelha <abdelamesquini@gmail.com>
2020-01-12 15:35:09 +0100

    Votings tests corrected (fix)
    All test can now run without problems.
    
    References #35

--- src/app/votings/votings.component.spec.ts
+++ src/app/votings/votings.component.spec.ts
@@ -2,5 +2,34 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { VotingsComponent } from './votings.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { VotingItemComponent } from './voting-item/voting-item.component';
+import { RouterTestingModule } from '@angular/router/testing';
+
+describe('VotingsComponent', () => {
+  let component: VotingsComponent;
+  let fixture: ComponentFixture<VotingsComponent>;
+  let element: HTMLElement;
 
 
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ VotingsComponent, VotingItemComponent ],
+      imports: [HttpClientTestingModule, RouterTestingModule]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(VotingsComponent);
+    component = fixture.componentInstance;
+    element = fixture.nativeElement;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+
+
+});
